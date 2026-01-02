7f614b1980e1a474104d49be3eeee34080ff573e
ubuntu-harshit <harshitsathone@gmail.com>
2026-01-03 01:35:25 +0530

    async service

--- src/main/java/com/harshit/NanoLinkResponder/controller/UrlController.java
+++ src/main/java/com/harshit/NanoLinkResponder/controller/UrlController.java
@@ -2,6 +2,8 @@ package com.harshit.NanoLinkResponder.controller;
 
 import com.harshit.NanoLinkResponder.service.UrlMappingService;
 import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -11,20 +13,34 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 @RestController
 public class UrlController {
+    Logger logger = LoggerFactory.getLogger(UrlController.class);
     @Autowired
     UrlMappingService urlMappingService;
     @GetMapping("/**")
-    public ResponseEntity<?> get(HttpServletRequest httpServletRequest){
+    public ResponseEntity<?> get(HttpServletRequest httpServletRequest) throws ExecutionException, InterruptedException {
         String shortUrl = httpServletRequest.getRequestURI().substring(1);
+        logger.info(shortUrl);
         System.out.println(shortUrl);
-        if(shortUrl.isBlank())
-           return ResponseEntity.badRequest().body("no url");
-        String longUrl = urlMappingService.getUrl(shortUrl);
+        if(shortUrl.isBlank()){
+            logger.info(" no url ");
+            return ResponseEntity.badRequest().body("no url");
+        }
+        CompletableFuture<String> future = urlMappingService.getUrl(shortUrl);
+        logger.info("async called");
+        while (true){
+            if(future.isDone()){
+                logger.info("async done");
+                break;
+            }
+        }
+        String longUrl = future.get();
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.setLocation(URI.create(longUrl));
-        return new ResponseEntity<Void>(httpHeaders,HttpStatus.MOVED_PERMANENTLY);
+        return new ResponseEntity<Void>(httpHeaders,HttpStatus.PERMANENT_REDIRECT);
     }
 }
--- src/main/java/com/harshit/NanoLinkResponder/service/UrlMappingService.java
+++ src/main/java/com/harshit/NanoLinkResponder/service/UrlMappingService.java
@@ -5,23 +5,32 @@ import com.harshit.NanoLinkResponder.exception.NoSuchUrlException;
 import com.harshit.NanoLinkResponder.mapper.UrlMappingMapper;
 import com.harshit.NanoLinkResponder.model.UrlMapping;
 import com.harshit.NanoLinkResponder.repo.UrlMappingRepo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
 
 @Service
 public class UrlMappingService {
+    Logger logger = LoggerFactory.getLogger(UrlMappingService.class);
     @Autowired
     private UrlMappingRepo urlMappingRepo;
     @Autowired
     private UrlMappingMapper urlMappingMapper;
 
-    public String getUrl(String shortUrl) throws NoSuchUrlException {
+    @Async
+    public CompletableFuture<String> getUrl(String shortUrl) throws NoSuchUrlException {
+        logger.info("in get url");
         Optional<UrlMapping> optional = urlMappingRepo.findByShortUrl(shortUrl);
-        if(optional.isEmpty()) throw new NoSuchUrlException(shortUrl+ "not found");
+        if (optional.isEmpty()) throw new NoSuchUrlException(shortUrl + "not found");
         UrlMappingDto dto = urlMappingMapper.urlToUrlDto(optional.get());
-        return dto.getLongUrl();
+        String longUrl = dto.getLongUrl();
+        logger.info("done get url");
+        return CompletableFuture.completedFuture(longUrl);
     }
 }
